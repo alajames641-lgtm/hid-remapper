3540a117b5666383fc4854789b9ddaba8fd02a5d
alajames641-lgtm <alajames641@gmail.com>
2026-01-23 00:04:58 +1100

    e

--- firmware/src/tinyusb_stuff.cc
+++ firmware/src/tinyusb_stuff.cc
@@ -3,25 +3,6 @@
  *
  * Copyright (c) 2022 Jacek Fedorynski
  * Copyright (c) 2019 Ha Thach (tinyusb.org)
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
- *
  */
 
 #include <tusb.h>
@@ -32,6 +13,7 @@
 #include "platform.h"
 #include "remapper.h"
 
+// --- STEALTH START: LOGITECH G102 IDENTITY ---
 #define USB_VID 0x046D         // Logitech Vendor ID
 #define USB_PID 0xC09D         // G102 Lightsync Product ID
 
@@ -46,14 +28,21 @@ tusb_desc_device_t desc_device = {
 
     .idVendor = USB_VID,
     .idProduct = USB_PID,
-    .bcdDevice = 0x2703,       // Stealth: Real G102 Firmware Revision
+    .bcdDevice = 0x2703,       // Stealth: Matches real G102 hardware rev
 
     .iManufacturer = 0x01,
     .iProduct = 0x02,
-    .iSerialNumber = 0x00,     // Stealth: Most gaming mice hide this
+    .iSerialNumber = 0x00,     // Stealth: 0 hides serial (standard for G102)
 
     .bNumConfigurations = 0x01,
 };
+
+char const* string_desc_arr[] = {
+    (const char[]){ 0x09, 0x04 },    // 0: English
+    "Logitech",                      // 1: Manufacturer
+    "G102 LIGHTSYNC Gaming Mouse",   // 2: Product
+};
+// --- STEALTH END ---
 
 const uint8_t configuration_descriptor0[] = {
     TUD_CONFIG_DESCRIPTOR(1, 2, 0, TUD_CONFIG_DESC_LEN + TUD_HID_DESC_LEN + TUD_HID_DESC_LEN, TUSB_DESC_CONFIG_ATT_REMOTE_WAKEUP, 100),
@@ -100,52 +89,25 @@ const uint8_t* configuration_descriptors[] = {
     configuration_descriptor5,
 };
 
-char const* string_desc_arr[] = {
-    (const char[]){ 0x09, 0x04 },  // 0: is supported language is English (0x0409)
-#ifdef PICO_RP2350
-    "RP2350",  // 1: Manufacturer
-#else
-    "RP2040",  // 1: Manufacturer
-#endif
-    "HID Remapper XXXX",  // 2: Product
-};
-
-// Invoked when received GET DEVICE DESCRIPTOR
-// Application return pointer to descriptor
 uint8_t const* tud_descriptor_device_cb() {
-    if ((our_descriptor->vid != 0) && (our_descriptor->pid != 0)) {
-        desc_device.idVendor = our_descriptor->vid;
-        desc_device.idProduct = our_descriptor->pid;
-    }
     return (uint8_t const*) &desc_device;
 }
 
-// Invoked when received GET CONFIGURATION DESCRIPTOR
-// Application return pointer to descriptor
-// Descriptor contents must exist long enough for transfer to complete
 uint8_t const* tud_descriptor_configuration_cb(uint8_t index) {
     return configuration_descriptors[our_descriptor->idx];
 }
 
-// Invoked when received GET HID REPORT DESCRIPTOR
-// Application return pointer to descriptor
-// Descriptor contents must exist long enough for transfer to complete
 uint8_t const* tud_hid_descriptor_report_cb(uint8_t itf) {
     if (itf == 0) {
         return our_descriptor->descriptor;
     } else if (itf == 1) {
         return config_report_descriptor;
     }
-
     return NULL;
 }
 
 static uint16_t _desc_str[32];
 
-const char id_chars[33] = "0123456789ABCDEFGHIJKLMNOPQRSTUV";
-
-// Invoked when received GET STRING DESCRIPTOR request
-// Application return pointer to descriptor, whose contents must exist long enough for transfer to complete
 uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
     uint8_t chr_count;
 
@@ -153,35 +115,22 @@ uint16_t const* tud_descriptor_string_cb(uint8_t index, uint16_t langid) {
         memcpy(&_desc_str[1], string_desc_arr[0], 2);
         chr_count = 1;
     } else {
-        // Note: the 0xEE index string is a Microsoft OS 1.0 Descriptors.
-        // https://docs.microsoft.com/en-us/windows-hardware/drivers/usbcon/microsoft-defined-usb-descriptors
-
         if (!(index < sizeof(string_desc_arr) / sizeof(string_desc_arr[0])))
             return NULL;
 
         const char* str = string_desc_arr[index];
-
-        // Cap at max char
         chr_count = strlen(str);
-        if (chr_count > 31)
-            chr_count = 31;
+        if (chr_count > 31) chr_count = 31;
 
-        // Convert ASCII string into UTF-16
         for (uint8_t i = 0; i < chr_count; i++) {
             _desc_str[1 + i] = str[i];
         }
-
-        if (index == 2) {
-            uint64_t unique_id = get_unique_id();
-            for (uint8_t i = 0; i < 4; i++) {
-                _desc_str[1 + chr_count - 4 + i] = id_chars[(unique_id >> (15 - i * 5)) & 0x1F];
-            }
-        }
+        
+        // REMOVED: Unique ID appending. 
+        // Real Logitech mice don't have random characters at the end of their name.
     }
 
-    // first byte is length (including header), second byte is string type
     _desc_str[0] = (TUSB_DESC_STRING << 8) | (2 * chr_count + 2);
-
     return _desc_str;
 }
 
@@ -206,7 +155,6 @@ void tud_hid_set_report_cb(uint8_t itf, uint8_t report_id, hid_report_type_t rep
 }
 
 void tud_hid_set_protocol_cb(uint8_t instance, uint8_t protocol) {
-    printf("tud_hid_set_protocol_cb %d %d\n", instance, protocol);
     boot_protocol_keyboard = (protocol == HID_PROTOCOL_BOOT);
     boot_protocol_updated = true;
 }
@@ -219,10 +167,5 @@ void tud_mount_cb() {
     }
 }
 
-void tud_suspend_cb(bool remote_wakeup_en) {
-    printf("tud_suspend_cb\n");
-}
-
-void tud_resume_cb() {
-    printf("tud_resume_cb\n");
-}
+void tud_suspend_cb(bool remote_wakeup_en) {}
+void tud_resume_cb() {}
